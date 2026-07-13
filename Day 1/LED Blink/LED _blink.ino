f40ce3421d890a9d3e0d2d890cb38306610c222d
Roshan Ayyalasomayajula <32754296+Roshan-AS@users.noreply.github.com>
2026-07-13 11:52:38 -0400

    Update LED _blink.ino

--- Day 1/LED Blink/LED _blink.ino
+++ Day 1/LED Blink/LED _blink.ino
@@ -21,7 +21,7 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
-
+LED_BUITLIN = 13;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
