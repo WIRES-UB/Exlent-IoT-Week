2620e00f27a22c4723aa91b048aa8047419cb63e
Roshan Ayyalasomayajula <32754296+Roshan-AS@users.noreply.github.com>
2026-07-13 11:57:30 -0400

    Fix variable name for LED pin in LED blink example

--- Day 1/LED Blink/LED _blink.ino
+++ Day 1/LED Blink/LED _blink.ino
@@ -21,17 +21,17 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
-const int LED_BUITLIN = 13;
+const int ledIn = 13;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledIn, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
+  digitalWrite(ledIn, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
+  digitalWrite(ledIn, LOW);   // change state of the LED by setting the pin to the LOW voltage level
   delay(1000);                      // wait for a second
 }
